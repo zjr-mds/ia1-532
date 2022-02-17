cb74ce62afd9625e9c7a30934d3b2479d1e7c2d3
Junrong Zhu <junrongz@student.ubc.ca>
2022-02-16 16:00:41 -0800

    Add additional line under app

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ df = movies[['Production_Budget', 'Running_Time_min',
 
 
 app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+server = app.server
+
 app.layout = html.Div([
     html.Iframe(
         id='scatter',
